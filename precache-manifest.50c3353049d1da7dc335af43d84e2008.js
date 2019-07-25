ba59cb5c7db596491130ab8aa6e5be12532c70f0
Taylor Jordan <taylornoel418@icloud.com>
2019-07-25 10:26:03 -0600

    Updates

---
+++ precache-manifest.50c3353049d1da7dc335af43d84e2008.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c146391de1bcfba174662b338a55213",
+    "url": "/portfolio-taylorjordan/index.html"
+  },
+  {
+    "revision": "7b3eb0c4dea032d37573",
+    "url": "/portfolio-taylorjordan/static/css/2.a881067f.chunk.css"
+  },
+  {
+    "revision": "9358cf4114cc9626f85a",
+    "url": "/portfolio-taylorjordan/static/css/main.6d9a1d06.chunk.css"
+  },
+  {
+    "revision": "7b3eb0c4dea032d37573",
+    "url": "/portfolio-taylorjordan/static/js/2.46d2f3a7.chunk.js"
+  },
+  {
+    "revision": "9358cf4114cc9626f85a",
+    "url": "/portfolio-taylorjordan/static/js/main.383c2d45.chunk.js"
+  },
+  {
+    "revision": "caaf10b7db1b386a1d39",
+    "url": "/portfolio-taylorjordan/static/js/runtime~main.16aa1ed6.js"
+  },
+  {
+    "revision": "a0909c5adfec1b05b03b39a40f92c173",
+    "url": "/portfolio-taylorjordan/static/media/002-disco-ball.a0909c5a.svg"
+  },
+  {
+    "revision": "8d02fad9b457184a3252362fdde40ed5",
+    "url": "/portfolio-taylorjordan/static/media/004-necklace.8d02fad9.svg"
+  },
+  {
+    "revision": "a126fdfdfc2eabc55237b492a2d9414a",
+    "url": "/portfolio-taylorjordan/static/media/006-envelope.a126fdfd.svg"
+  },
+  {
+    "revision": "c5db80257adc5f8b61158420ae17c39d",
+    "url": "/portfolio-taylorjordan/static/media/011-wig.c5db8025.svg"
+  },
+  {
+    "revision": "7b8fed094a5ac6183a5d455b9b02d6ed",
+    "url": "/portfolio-taylorjordan/static/media/alesia-kazantceva-iYoAPaSPBKI-unsplash.7b8fed09.jpg"
+  },
+  {
+    "revision": "2efd6f768250118838cb73112a2474e6",
+    "url": "/portfolio-taylorjordan/static/media/checkyoself.2efd6f76.png"
+  },
+  {
+    "revision": "7cda353c31f80a91d669bee9893ca12b",
+    "url": "/portfolio-taylorjordan/static/media/fitlit.7cda353c.png"
+  },
+  {
+    "revision": "751baa7f50357fd69fb1865ccab29212",
+    "url": "/portfolio-taylorjordan/static/media/lightside.751baa7f.png"
+  },
+  {
+    "revision": "fcb9c9bb4cfcb0d169666e1ad078855a",
+    "url": "/portfolio-taylorjordan/static/media/movietracker.fcb9c9bb.png"
+  },
+  {
+    "revision": "6ff8e1ad24c886f6770d6dc765119319",
+    "url": "/portfolio-taylorjordan/static/media/tay-shot.6ff8e1ad.jpg"
+  }
+]);
